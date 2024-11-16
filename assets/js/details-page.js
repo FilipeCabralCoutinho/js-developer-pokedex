3fa0a80111438a10c0366df711dfe1847f446de9
filipeCoutinho <filipeccoutinho2017@gmail.com>
2024-11-16 10:03:49 -0300

    creation page details html css js

---
+++ assets/js/details-page.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+<Ol id="pageDetailsPokemon">
+    <li class="pokemon grass">
+        <div class="top">
+            <span class="name">Bulbassaur</span>
+            <span class="number">#1</span>
+        </div>
+        <div class="detail">
+            <ol class="types">
+                <li class="type grass">grass</li>
+                <li class="type poison">poison</li>
+            </ol>
+            <div class="img_pokemon"><img src="/bulbasaur-seeklogo.svg" alt=""></div>
+        </div>
+        <ol class="moves">
+            <p>Moves</p>
+            <li class="move">Cut</li>
+            <li class="move">Chicote</li>
+            <li class="move">Poison Stings</li>
+            <li class="move">Tackle</li>
+        </ol>
+    </li>
+</Ol>
